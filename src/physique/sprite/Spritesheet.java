4638942544d8cede13951cf642a58bfd702788ae
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-05-09 22:33:00 +0200

    Init Spritesheet

---
+++ src/physique/sprite/Spritesheet.java
@@ -0,0 +1,20 @@
+package physique.sprite;
+
+public class Spritesheet {
+
+	private String name;
+	
+	public Spritesheet(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
